3bdc4b9ac05ff293059c727b3c88055970db3c77
salikbubu <salikbula@gmail.com>
2023-05-24 13:13:31 +0200

    ..

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,28 @@
 import './App.css';
+
+// components
 import Container from './components/Container';
 import Gallery from './components/Gallery';
+
+// Array
 import GalleryArr from './data/GalleryArr';
 
 function App() {
   return (
-    <div className="">
+    <>
       <Container>
         {GalleryArr.map((gallery, index) => (
+          // Her indsættes "Gallery.tsx" Komponent
         <Gallery
           key={index}
           id={`gallery${index + 1}`}
           bigImage={gallery.bigImage}
           smallImages={gallery.images}
-          onClick={() => {
-            // Handle gallery click event
-            console.log(`Gallery ${index + 1} clicked`);
-          }}
+          onClick={() => {console.log(`Gallery ${index + 1} clicked`);}}
         />
       ))}
       </Container>
-    </div>
+    </>
   );
 }
 
